6fed7f1337c87edcf70033caed161ffa07192290
Karan <karan@karans-air.home>
2023-10-21 19:19:25 +0200

    Post Service Redesigned Successful

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,79 @@
+import {useState} from "react";
 import "./App.css";
-
+import postService from "./services/postService";
 function App() {
+	// Raw Data
+	const doc = {
+		title: "How To Get All Document",
+		content: "lorem lorem lorem lorem lorem",
+		featuredImage: "http://featuredimage.jpg",
+		status: "active",
+		userId: "user321j",
+	};
+
+	// Create Document
+	const createDocument = async () => {
+		try {
+			const document = await postService.createDocument(doc);
+			if (document) console.log(document);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	// Document ID
+	const document_Id = "6533fad310b4dd715e45";
+
+	// Update Document
+	const updateDocument = async () => {
+		try {
+			const document = await postService.updateDocument(document_Id, doc);
+			if (document) console.log(document);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	// Delete Document
+	const deleteDocument = async () => {
+		try {
+			const document = await postService.deleteDocument(document_Id);
+			if (document) {
+				console.log(document, "Deleted Successfully");
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	// List Document - Get Single Document
+	const listDocument = async () => {
+		try {
+			const document = await postService.listDocument(document_Id);
+			if (document) {
+				console.log(document);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	// List Documents - Get All Documents
+	const listDocuments = async () => {
+		try {
+			const data = await postService.listDocuments();
+			console.log(data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	
+
 	return (
 		<div className=" bg-slate-300">
 			<h1 className=" mt-36 text-4xl text-gray-900 text-center">Vite React</h1>
+			<button onClick={listDocuments}> Get All Document</button>
 		</div>
 	);
 }
---
+++ src/services/postService.js
@@ -0,0 +1,85 @@
+import {Client, ID, Databases} from "appwrite";
+import environment from "../environment/environment";
+
+class PostService {
+	client = new Client();
+	databases;
+
+	// Settings Database
+	constructor() {
+		this.client
+			.setEndpoint(environment.appwriteUrl)
+			.setProject(environment.appwriteProjectId);
+		this.databases = new Databases(this.client);
+	}
+
+	// Create Document @Successful
+	async createDocument({title, content, featuredImage, status, userId}) {
+		try {
+			return await this.databases.createDocument(
+				environment.appwriteDatabseId,
+				environment.appwriteCollectionId,
+				ID.unique(),
+				{title, content, featuredImage, status, userId}
+			);
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	// Update Document @Successful
+	async updateDocument(document_Id, {title, content, featuredImage, status}) {
+		try {
+			return await this.databases.updateDocument(
+				environment.appwriteDatabseId,
+				environment.appwriteCollectionId,
+				document_Id,
+				{title, content, featuredImage, status}
+			);
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	// Delete Document @Sucessful
+	async deleteDocument(document_Id) {
+		try {
+			await this.databases.deleteDocument(
+				environment.appwriteDatabseId,
+				environment.appwriteCollectionId,
+				document_Id
+			);
+			return true;
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	// List Document @Successful - Get Single Document
+	async listDocument(document_Id) {
+		try {
+			return await this.databases.getDocument(
+				environment.appwriteDatabseId,
+				environment.appwriteCollectionId,
+				document_Id
+			);
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	// List Documents @Pending - Get All Documents
+	async listDocuments() {
+		try {
+			return await this.databases.listDocuments(
+				environment.appwriteDatabseId,
+				environment.appwriteCollectionId
+			);
+		} catch (error) {
+			throw error;
+		}
+	}
+}
+
+const postService = new PostService();
+export default postService;
